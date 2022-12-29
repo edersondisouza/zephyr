dc2be337b3715e21557aebd00352dc0c0e829d06
NagarajuGundimi <nagaraju.gundimi@intel.com>
2022-12-29 03:29:54 -0500

    ite: Defined GPIO ports and delta changes for Zephyr v3.2
    Defined GPIO ports and delta changes for Zephyr v3.2
    
    Signed-off-by: NagarajuGundimi <nagaraju.gundimi@intel.com>

---
+++ soc/riscv/riscv-ite/common/soc_pins.h
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_ITE_GPIOS_H_
+#define _SOC_ITE_GPIOS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Begin Port A */
+#define ITE_GPIO_GPA0	(0U)
+#define ITE_GPIO_GPA1	(1U)
+#define ITE_GPIO_GPA2	(2U)
+#define ITE_GPIO_GPA3	(3U)
+#define ITE_GPIO_GPA4	(4U)
+#define ITE_GPIO_GPA5	(5U)
+#define ITE_GPIO_GPA6	(6U)
+#define ITE_GPIO_GPA7	(7U)
+/* End Port A */
+
+/* Begin Port B */
+#define ITE_GPIO_GPB0	(0U)
+#define ITE_GPIO_GPB1	(1U)
+#define ITE_GPIO_GPB2	(2U)
+#define ITE_GPIO_GPB3	(3U)
+#define ITE_GPIO_GPB4	(4U)
+#define ITE_GPIO_GPB5	(5U)
+#define ITE_GPIO_GPB6	(6U)
+#define ITE_GPIO_GPB7	(7U)
+/* End Port B */
+
+/* Begin Port C */
+#define ITE_GPIO_GPC0	(0U)
+#define ITE_GPIO_GPC1	(1U)
+#define ITE_GPIO_GPC2	(2U)
+#define ITE_GPIO_GPC3	(3U)
+#define ITE_GPIO_GPC4	(4U)
+#define ITE_GPIO_GPC5	(5U)
+#define ITE_GPIO_GPC6	(6U)
+#define ITE_GPIO_GPC7	(7U)
+/* End Port C */
+
+/* Begin Port D */
+#define ITE_GPIO_GPD0	(0U)
+#define ITE_GPIO_GPD1	(1U)
+#define ITE_GPIO_GPD2	(2U)
+#define ITE_GPIO_GPD3	(3U)
+#define ITE_GPIO_GPD4	(4U)
+#define ITE_GPIO_GPD5	(5U)
+#define ITE_GPIO_GPD6	(6U)
+#define ITE_GPIO_GPD7	(7U)
+/* End Port D */
+
+/* Begin Port E */
+#define ITE_GPIO_GPE0	(0U)
+#define ITE_GPIO_GPE1	(1U)
+#define ITE_GPIO_GPE2	(2U)
+#define ITE_GPIO_GPE3	(3U)
+#define ITE_GPIO_GPE4	(4U)
+#define ITE_GPIO_GPE5	(5U)
+#define ITE_GPIO_GPE6	(6U)
+#define ITE_GPIO_GPE7	(7U)
+/* End Port E */
+
+/* Begin Port F */
+#define ITE_GPIO_GPF0	(0U)
+#define ITE_GPIO_GPF1	(1U)
+#define ITE_GPIO_GPF2	(2U)
+#define ITE_GPIO_GPF3	(3U)
+#define ITE_GPIO_GPF4	(4U)
+#define ITE_GPIO_GPF5	(5U)
+#define ITE_GPIO_GPF6	(6U)
+#define ITE_GPIO_GPF7	(7U)
+/* End Port F */
+
+/* Begin Port G */
+#define ITE_GPIO_GPG0	(0U)
+#define ITE_GPIO_GPG1	(1U)
+#define ITE_GPIO_GPG2	(2U)
+#define ITE_GPIO_GPG3	(3U)
+#define ITE_GPIO_GPG4	(4U)
+#define ITE_GPIO_GPG5	(5U)
+#define ITE_GPIO_GPG6	(6U)
+#define ITE_GPIO_GPG7	(7U)
+/* End Port G */
+
+/* Begin Port H */
+#define ITE_GPIO_GPH0	(0U)
+#define ITE_GPIO_GPH1	(1U)
+#define ITE_GPIO_GPH2	(2U)
+#define ITE_GPIO_GPH3	(3U)
+#define ITE_GPIO_GPH4	(4U)
+#define ITE_GPIO_GPH5	(5U)
+#define ITE_GPIO_GPH6	(6U)
+#define ITE_GPIO_GPH7	(7U)
+/* End Port H */
+
+/* Begin Port I */
+#define ITE_GPIO_GPI0	(0U)
+#define ITE_GPIO_GPI1	(1U)
+#define ITE_GPIO_GPI2	(2U)
+#define ITE_GPIO_GPI3	(3U)
+#define ITE_GPIO_GPI4	(4U)
+#define ITE_GPIO_GPI5	(5U)
+#define ITE_GPIO_GPI6	(6U)
+#define ITE_GPIO_GPI7	(7U)
+/* End Port I */
+
+/* Begin Port J */
+#define ITE_GPIO_GPJ0	(0U)
+#define ITE_GPIO_GPJ1	(1U)
+#define ITE_GPIO_GPJ2	(2U)
+#define ITE_GPIO_GPJ3	(3U)
+#define ITE_GPIO_GPJ4	(4U)
+#define ITE_GPIO_GPJ5	(5U)
+#define ITE_GPIO_GPJ6	(6U)
+#define ITE_GPIO_GPJ7	(7U)
+/* End Port J */
+
+/* Begin Port K */
+#define ITE_GPIO_GPK0	(0U)
+#define ITE_GPIO_GPK1	(1U)
+#define ITE_GPIO_GPK2	(2U)
+#define ITE_GPIO_GPK3	(3U)
+#define ITE_GPIO_GPK4	(4U)
+#define ITE_GPIO_GPK5	(5U)
+#define ITE_GPIO_GPK6	(6U)
+#define ITE_GPIO_GPK7	(7U)
+/* End Port K */
+
+/* Begin Port L */
+#define ITE_GPIO_GPL0	(0U)
+#define ITE_GPIO_GPL1	(1U)
+#define ITE_GPIO_GPL2	(2U)
+#define ITE_GPIO_GPL3	(3U)
+#define ITE_GPIO_GPL4	(4U)
+#define ITE_GPIO_GPL5	(5U)
+#define ITE_GPIO_GPL6	(6U)
+#define ITE_GPIO_GPL7	(7U)
+/* End Port L */
+
+/* Begin Port M */
+#define ITE_GPIO_GPM0	(0U)
+#define ITE_GPIO_GPM1	(1U)
+#define ITE_GPIO_GPM2	(2U)
+#define ITE_GPIO_GPM3	(3U)
+#define ITE_GPIO_GPM4	(4U)
+#define ITE_GPIO_GPM5	(5U)
+#define ITE_GPIO_GPM6	(6U)
+#define ITE_GPIO_GPM7	(7U)
+/* End Port M */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC_ITE_GPIOS_H_ */
