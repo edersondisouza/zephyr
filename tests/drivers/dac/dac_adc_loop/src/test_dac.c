67eca5e54175b407034d00bd632a94ad740f8d13
Yusmed <muhammed.ahmed@intel.com>
2023-12-07 19:59:36 -0800

    ADC test completely made generic using DAC

--- tests/drivers/dac/dac_adc_loop/src/test_dac.c
+++ tests/drivers/dac/dac_adc_loop/src/test_dac.c
@@ -4,27 +4,12 @@
 #include <zephyr/ztest.h>
 #include <stdio.h>
 
-/*#if defined(CONFIG_BOARD_FRDM_K64F)
-
-#define DAC_DEVICE_NODE		DT_NODELABEL(dac0)
-#define DAC_RESOLUTION		12
-#define DAC_CHANNEL_ID		0
-
-#define ADC_DEVICE_NODE		DT_NODELABEL(adc0)
-#define ADC_RESOLUTION		12
-#define ADC_GAIN		ADC_GAIN_1
-#define ADC_REFERENCE		ADC_REF_INTERNAL
-#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
-#define ADC_CHANNEL_ID		23
-
-#else
-#error "Unsupported board."
-#endif
-*/
-// check drivers, dac.h, check macros
+#if DT_NODE_HAS_PROP(DT_PATH(zephyr_user), dac)
 #define DAC_DEVICE_NODE		DT_PROP(DT_PATH(zephyr_user), dac)// DT_NODELABEL(dac0)
-//#define ADC_DEVICE_NODE 	DT_PROP(DT_PATH(zephyr_user), io_channels)// DT_NODELABEL(adc0)
+#endif
+
 #define ADC_DEVICE_NODE DT_PHANDLE(DT_PATH(zephyr_user), io_channels)
+
 
 static const struct dac_channel_cfg dac_ch_cfg = {
 	.channel_id = DT_PROP(DT_PATH(zephyr_user), dac_channel_id),
@@ -32,17 +17,11 @@ static const struct dac_channel_cfg dac_ch_cfg = {
 	.buffered = true
 };
 
-static const struct adc_channel_cfg adc_ch_cfg = ADC_CHANNEL_CFG_DT(DT_CHILD(ADC_DEVICE_NODE, channel_e));/*{
-	.gain             = ADC_GAIN,
-	.reference        = ADC_REFERENCE,
-	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id       = ADC_CHANNEL_ID,*/
-
+static const struct adc_channel_cfg adc_ch_cfg = ADC_CHANNEL_CFG_DT(DT_CHILD(ADC_DEVICE_NODE, channel_e));
 
 static const struct device *init_dac(void)
 {
 	int ret;
-	/*TODO: DAC_DEVICE_NODE is commented out */
 	const struct device *const dac_dev = DEVICE_DT_GET(DAC_DEVICE_NODE);
 
 	zassert_true(device_is_ready(dac_dev), "DAC device is not ready");
@@ -57,7 +36,6 @@ static const struct device *init_dac(void)
 static const struct device *init_adc(void)
 {
 	int ret;
-	/*TODO: ADC_DEVICE_NODE is commented out */
 	const struct device *const adc_dev = DEVICE_DT_GET(ADC_DEVICE_NODE);
 
 	zassert_true(device_is_ready(adc_dev), "ADC device is not ready");
@@ -80,44 +58,20 @@ static int test_dac_to_adc(void)
 		return TC_FAIL;
 	}
 
-
-	printf("ADC: ");
-	//printf(DAC_NODE);
-
-	printf("--------------------------ADC STUFF------------------------------\n");
-	printf("ACQ TIME: %d\n", adc_ch_cfg.acquisition_time);
-	printf("DEFAULT ADCGAIN: %d\n", ADC_GAIN_1);
-	printf("DEFAULT REF: %d\n", ADC_REF_INTERNAL);
-	//printf("CHANNEL_ID: %d or %d\n", adc_ch_cfg.channel_id, BIT(DT_REG_ADDR(ADC_DEVICE_NODE)));
-
-	printf("DIFFERENTIAL: %d\n", adc_ch_cfg.differential);
-	printf("GAIN: %d\n", adc_ch_cfg.gain);
-	printf("REFERENCE: %d\n", adc_ch_cfg.reference);
-	printf("RESOLUTION: %d\n", DT_PROP(DT_CHILD(ADC_DEVICE_NODE, channel_e), zephyr_resolution));
-
-	printf("---------------------------DAC STUFF-------------------------------\n");
-
-	printf("RESOLUTION: %d\n", dac_ch_cfg.resolution);
-	printf("CHANNEL_ID: %d\n", dac_ch_cfg.channel_id);
-	printf("WRITE CHANNEL ID VALUE: %d\n", DT_PROP(DT_PATH(zephyr_user), dac_channel_id));
-	printf("BUFFERED: %d\n", dac_ch_cfg.buffered);	
-
 	int write_val = (1U << dac_ch_cfg.resolution) / 2;
 
 	ret = dac_write_value(dac_dev, DT_PROP(DT_PATH(zephyr_user), dac_channel_id), write_val); // half value
-
-	
-
-	printf("DAC WRITE VALUE: %d\n", write_val);
 
 	zassert_equal(ret, 0, "dac_write_value() failed with code %d", ret);
 
 	k_sleep(K_MSEC(10));
 
+	
+
 	static int32_t m_sample_buffer[1];
-	static const struct adc_sequence sequence = {
+	struct adc_sequence sequence = {
 		/*TODO: you are getting 'zephyr,channel-id' prop from adc0, but adc0 doesn't have it. Which node does have it? */
-		.channels    = BIT(23),
+		.channels    = BIT(adc_ch_cfg.channel_id),
 		.buffer      = &m_sample_buffer,
 		.buffer_size = sizeof(m_sample_buffer),
 		/*TODO: you are getting 'zephyr,resolution' prop from adc0, but adc0 doesn't have it. Which node does have it? */
@@ -126,9 +80,9 @@ static int test_dac_to_adc(void)
 
 	ret = adc_read(adc_dev, &sequence);
 	
-       	float val_mv = m_sample_buffer[0]; 
+   	float val_mv = m_sample_buffer[0]; 
 
-	printf ("VAL_MV: %f\n", val_mv);	
+	//printf ("VAL_MV: %f\n", val_mv);	
 	val_mv  = (val_mv/4096 * 3.3); 
 
 
